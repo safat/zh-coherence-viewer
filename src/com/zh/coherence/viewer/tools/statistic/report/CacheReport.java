eb71f3d2cdaf2fd6db09653209f20fe84b82c267
zhivgorodsky <zhivgorodsky@e121c064-bac4-a317-bacd-f7ff62a0eed7>
2012-05-18 20:30:49 +0000

    added JMX cache info

--- src/com/zh/coherence/viewer/tools/statistic/report/CacheReport.java
+++ src/com/zh/coherence/viewer/tools/statistic/report/CacheReport.java
@@ -2,6 +2,7 @@ package com.zh.coherence.viewer.tools.statistic.report;
 
 import com.zh.coherence.viewer.jmx.JMXManager;
 import com.zh.coherence.viewer.tools.statistic.report.cache.CacheInfo;
+import com.zh.coherence.viewer.tools.statistic.report.cache.CacheNodeInfo;
 
 import javax.management.MBeanServerConnection;
 import javax.management.ObjectName;
@@ -16,18 +17,51 @@ public class CacheReport {
     public void updateData() {
         try{
             data.clear();
+            totalUnits = 0;
 
             MBeanServerConnection server = JMXManager.getInstance().getServer();
-            Set<String> ret = new HashSet<String>();
             Set<ObjectName> cacheNamesSet = server.queryNames(new ObjectName("Coherence:type=Cache,*"), null);
             for (Object aCacheNamesSet : cacheNamesSet) {
                 ObjectName cacheNameObjName = (ObjectName) aCacheNamesSet;
-                ret.add(cacheNameObjName.getKeyProperty("name"));
-                System.err.println("size: " + server.getAttribute(cacheNameObjName, "Size"));
+                String name = cacheNameObjName.getKeyProperty("name");
+                if(!data.containsKey(name)){
+                    data.put(name, new CacheInfo(name));
+                }
+                CacheNodeInfo cacheNodeInfo = new CacheNodeInfo();
+                cacheNodeInfo.setName(cacheNameObjName.getKeyProperty("nodeId"));
+                Integer size = (Integer) server.getAttribute(cacheNameObjName, "Size");
+                cacheNodeInfo.setSize(size);
+                totalUnits += size;
+                Long totalPuts = (Long) server.getAttribute(cacheNameObjName, "TotalPuts");
+                cacheNodeInfo.setTotalPuts(totalPuts);
+                Long totalGets = (Long) server.getAttribute(cacheNameObjName, "TotalGets");
+                cacheNodeInfo.setTotalGets(totalGets);
+                Long cacheHits = (Long) server.getAttribute(cacheNameObjName, "CacheHits");
+                cacheNodeInfo.setCacheHits(cacheHits);
+                Double averageGetMillis = (Double) server.getAttribute(cacheNameObjName, "AverageGetMillis");
+                cacheNodeInfo.setAverageGetMillis(averageGetMillis);
+
+                data.get(name).addCacheNodeInfo(cacheNodeInfo);
             }
 
         }catch (Exception ex){
             ex.printStackTrace();
         }
+    }
+
+    public List<CacheInfo> getData() {
+        return new ArrayList<CacheInfo>(data.values());
+    }
+
+    public void setData(Map<String, CacheInfo> data) {
+        this.data = data;
+    }
+
+    public long getTotalUnits() {
+        return totalUnits;
+    }
+
+    public void setTotalUnits(long totalUnits) {
+        this.totalUnits = totalUnits;
     }
 }
--- src/com/zh/coherence/viewer/tools/statistic/report/cache/CacheInfo.java
+++ src/com/zh/coherence/viewer/tools/statistic/report/cache/CacheInfo.java
@@ -1,4 +1,73 @@
 package com.zh.coherence.viewer.tools.statistic.report.cache;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Живко
+ * Date: 18.05.12
+ * Time: 21:44
+ */
 public class CacheInfo {
+    private List<CacheNodeInfo> nodes = new ArrayList<CacheNodeInfo>();
+
+    private int size = 0;
+
+    private long totalPuts = 0;
+
+    private long totalGets = 0;
+
+    private String name;
+
+    public CacheInfo(String name) {
+        this.name = name;
+    }
+
+    public void addCacheNodeInfo(CacheNodeInfo info){
+        nodes.add(info);
+        size += info.getSize();
+        totalPuts += info.getTotalPuts();
+        totalGets += info.getTotalGets();
+    }
+
+    public List<CacheNodeInfo> getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(List<CacheNodeInfo> nodes) {
+        this.nodes = nodes;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public long getTotalGets() {
+        return totalGets;
+    }
+
+    public void setTotalGets(long totalGets) {
+        this.totalGets = totalGets;
+    }
+
+    public long getTotalPuts() {
+        return totalPuts;
+    }
+
+    public void setTotalPuts(long totalPuts) {
+        this.totalPuts = totalPuts;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
