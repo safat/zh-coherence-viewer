ce28632c1bf5f0ab67be67d69c59eaeebba06238
zhivgorodsky <zhivgorodsky@e121c064-bac4-a317-bacd-f7ff62a0eed7>
2012-06-06 06:38:23 +0000

    fix jmx report bug, add debug info

--- trunk/config/run.bat
+++ trunk/config/run.bat
@@ -1,1 +1,1 @@
-java -Xms64m -Xmx512m -splash:config/splash.png -cp ".;./lib/*;./coherence-lib/*;./zh_coherence_viewer.jar;./user-files;./user-files/*" com.zh.coherence.viewer.BootLoader
+java -Xms64m -Xmx512m -splash:config/splash.png -cp ".;./lib/*;./config;./coherence-lib/*;./zh_coherence_viewer.jar;./user-files;./user-files/*" com.zh.coherence.viewer.BootLoader
