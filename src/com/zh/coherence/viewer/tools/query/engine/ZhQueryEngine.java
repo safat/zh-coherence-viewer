4172b5270973b900d41b92fab5d9e3072a09e3e7
zhivgorodsky <zhivgorodsky@e121c064-bac4-a317-bacd-f7ff62a0eed7>
2012-06-19 22:19:40 +0000

    removed lib

--- src/com/zh/coherence/viewer/tools/query/engine/ZhQueryEngine.java
+++ src/com/zh/coherence/viewer/tools/query/engine/ZhQueryEngine.java
@@ -2,16 +2,12 @@ package com.zh.coherence.viewer.tools.query.engine;
 
 import com.zh.coherence.viewer.tools.query.QueryEngine;
 import com.zh.coherence.viewer.tools.query.QueryResult;
-import net.sf.jsqlparser.JSQLParserException;
-import net.sf.jsqlparser.parser.CCJSqlParserManager;
-import net.sf.jsqlparser.statement.Statement;
-import net.sf.jsqlparser.statement.select.Select;
 
 import java.io.Reader;
 import java.io.StringReader;
 
 public class ZhQueryEngine implements QueryEngine{
-    CCJSqlParserManager manager = new CCJSqlParserManager();
+//    CCJSqlParserManager manager = new CCJSqlParserManager();
 
     @Override
     public String getJavaSource(String query) {
@@ -27,18 +23,18 @@ public class ZhQueryEngine implements QueryEngine{
     public QueryResult execute(String query) {
         Reader reader = new StringReader(query);
 
-        try {
-            Statement statement = manager.parse(reader);
-            System.err.println("statement: " + statement);
-            if(statement instanceof Select){
-                Select sel = (Select) statement;
-                SelectProcessor processor = new SelectProcessor();
-                sel.getSelectBody().accept(processor);
-            }
-
-        } catch (JSQLParserException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            Statement statement = manager.parse(reader);
+//            System.err.println("statement: " + statement);
+//            if(statement instanceof Select){
+//                Select sel = (Select) statement;
+//                SelectProcessor processor = new SelectProcessor();
+//                sel.getSelectBody().accept(processor);
+//            }
+//
+//        } catch (JSQLParserException e) {
+//            e.printStackTrace();
+//        }
 
         return null;
     }
--- src/com/zh/coherence/viewer/tools/query/parser/Test.java
+++ src/com/zh/coherence/viewer/tools/query/parser/Test.java
@@ -1,50 +1,50 @@
-package com.zh.coherence.viewer.tools.query.parser;
-
-import net.sf.jsqlparser.parser.CCJSqlParserManager;
-import net.sf.jsqlparser.statement.Statement;
-import net.sf.jsqlparser.statement.select.*;
-
-import java.io.Reader;
-import java.io.StringReader;
-
-/**
- * Created by IntelliJ IDEA.
- * User: Живко
- * Date: 06.06.12
- * Time: 22:45
- */
-public class Test implements SelectVisitor {
-    public static void main(String[] args) {
-        try {
-            new Test();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public Test() throws Exception {
-        CCJSqlParserManager manager = new CCJSqlParserManager();
-        Reader reader = new StringReader("select count(*) as c from \"cache0\" where id = 'test'");
-        Statement statement = manager.parse(reader);
-        System.err.println("statement: " + statement);
-        if(statement instanceof Select){
-            Select sel = (Select) statement;
-            sel.getSelectBody().accept(this);
-        }
-    }
-
-    @Override
-    public void visit(PlainSelect plainSelect) {
-        System.err.println("visit select: " + plainSelect);
-        System.err.println("plainSelect.getSelectItems(): " + plainSelect.getSelectItems());
-        SelectExpressionItem exp = (SelectExpressionItem) plainSelect.getSelectItems().get(0);
-//        exp.getExpression().accept(new EV());
-//        System.err.println("exp: " + );
-        System.err.println("exp: " + exp.getAlias());
-    }
-
-    @Override
-    public void visit(Union union) {
-        System.err.println("visit union: " + union);
-    }
-}
+//package com.zh.coherence.viewer.tools.query.parser;
+//
+//import net.sf.jsqlparser.parser.CCJSqlParserManager;
+//import net.sf.jsqlparser.statement.Statement;
+//import net.sf.jsqlparser.statement.select.*;
+//
+//import java.io.Reader;
+//import java.io.StringReader;
+//
+///**
+// * Created by IntelliJ IDEA.
+// * User: Живко
+// * Date: 06.06.12
+// * Time: 22:45
+// */
+//public class Test implements SelectVisitor {
+//    public static void main(String[] args) {
+//        try {
+//            new Test();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public Test() throws Exception {
+//        CCJSqlParserManager manager = new CCJSqlParserManager();
+//        Reader reader = new StringReader("select count(*) as c from \"cache0\" where id = 'test'");
+//        Statement statement = manager.parse(reader);
+//        System.err.println("statement: " + statement);
+//        if(statement instanceof Select){
+//            Select sel = (Select) statement;
+//            sel.getSelectBody().accept(this);
+//        }
+//    }
+//
+//    @Override
+//    public void visit(PlainSelect plainSelect) {
+//        System.err.println("visit select: " + plainSelect);
+//        System.err.println("plainSelect.getSelectItems(): " + plainSelect.getSelectItems());
+//        SelectExpressionItem exp = (SelectExpressionItem) plainSelect.getSelectItems().get(0);
+////        exp.getExpression().accept(new EV());
+////        System.err.println("exp: " + );
+//        System.err.println("exp: " + exp.getAlias());
+//    }
+//
+//    @Override
+//    public void visit(Union union) {
+//        System.err.println("visit union: " + union);
+//    }
+//}
