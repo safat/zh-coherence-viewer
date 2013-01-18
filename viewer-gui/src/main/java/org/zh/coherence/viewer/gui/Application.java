186d9ba88a45969ad11a2b1fce134e907bd00fea
zhivgorodsky <zhivgorodsky@e121c064-bac4-a317-bacd-f7ff62a0eed7>
2013-01-18 21:11:41 +0000

    removed felix added JPF

--- viewer-gui/src/main/java/org/zh/coherence/viewer/gui/Application.java
+++ viewer-gui/src/main/java/org/zh/coherence/viewer/gui/Application.java
@@ -1,10 +1,6 @@
 package org.zh.coherence.viewer.gui;
 
 import bibliothek.gui.dock.common.*;
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceEvent;
-import org.osgi.framework.ServiceListener;
 import org.zh.coherence.viewer.gui.menu.ApplicationMenu;
 
 import javax.swing.*;
@@ -16,11 +12,11 @@ import java.awt.*;
  * Date: 12.12.12
  * Time: 21:55
  */
-public class Application implements BundleActivator, ServiceListener {
+public class Application {
 
     private JFrame frame = new JFrame();
 
-    public Application() {
+    public Application() { //todo try to run the application
         initFrame();
     }
 
@@ -77,35 +73,5 @@ public class Application implements BundleActivator, ServiceListener {
         bg.setBackground(color);
 
         return new DefaultSingleCDockable(title, title, bg);
-    }
-
-    @Override
-    public void start(BundleContext context) throws Exception {
-        System.out.println("Starting to listen for service events.");
-        context.addServiceListener(this);
-        initFrame();
-    }
-
-    @Override
-    public void stop(BundleContext context) throws Exception {
-        context.removeServiceListener(this);
-        System.out.println("Stopped listening for service events.");
-    }
-
-    @Override
-    public void serviceChanged(ServiceEvent event) {
-        String[] objectClass = (String[])
-                event.getServiceReference().getProperty("objectClass");
-
-        if (event.getType() == ServiceEvent.REGISTERED) {
-            System.out.println(
-                    "Ex1: Service of type " + objectClass[0] + " registered.");
-        } else if (event.getType() == ServiceEvent.UNREGISTERING) {
-            System.out.println(
-                    "Ex1: Service of type " + objectClass[0] + " unregistered.");
-        } else if (event.getType() == ServiceEvent.MODIFIED) {
-            System.out.println(
-                    "Ex1: Service of type " + objectClass[0] + " modified.");
-        }
     }
 }
